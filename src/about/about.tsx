e59c8d54c2f2da2321eb52ecae25f4168f0df914
Vivian Chen <vfc999@gmail.com>
2025-02-25 14:50:05 -0500

    Updated website

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -11,14 +11,16 @@ const About = () => {
       <div className='description'>
         <h3>Vivian Faye Chen</h3>
         <p>
-          Hi there! My name is Vivian and I’m a photographer from Southern California that relocated to New York about a year ago. I am currently working on my doctorate in Physical Therapy and am aiming towards a career in dance medicine working with the Broadway community. I would ultimately like to integrate my love for photography, healthcare, and theatre since each have shaped my life so drastically.
+           Hi there! My name is Vivian and I’m a photographer from Southern California that relocated to New York in 2019. I’ve been shooting for over 15 years and would say I’m an amateur photographer with an advanced eye.
         </p>
         <p>
-          I began playing around with photography in late high school as a way to break out of my awkward pre-teen shell. It was a way to encourage myself to walk the extra mile or explore the unknown spot because I never knew where my next shot would come from. It was a central component in fostering my adventurous spirit and it continues to push me to see and do more.
+           I began experimenting with photography as a camera shy high schooler who would rather be behind the camera instead of in front of it. Eventually this hideaway became my why. It’s why I would wake up early for a hike or why I’ll walk the extra mile for the better view. It’s  why I push myself to always do and see more; I’m always in pursuit of the next shot.
         </p> 
         <p>
-          I can’t say there is one particular subject matter I prefer shooting. I have had the wonderful opportunity to shoot all around the world with a wide array of people. While I do not actively seek out projects/work due to my studies, I do my best to accommodate anyone who finds my work  and wants to shoot with me. I have done everything from headshots to cosplay shoots to product shoots and would love to continue diversifying. 
+           My subject matter tends to vary. My life has led me toward more landscape and street photography, both of which I’m still growing with. I particularly love working with the interplay of geometric shapes and contrasting lights to create deeply atmospheric photographs. I have also done my share of portrait shots, mostly cosplay and graduation but have recently started experimenting with more studio work as well.
         </p>
+        <p>
+            By day (technically night) I’m am a Physical Therapist working on Broadway. I love theater and I would love to marry my passion for theatre and healthcare with my photography. I am very open to collaboration ideas and I am currently brainstorming a project that highlights dancer movement and strength. Otherwise my main focus has been getting my print shop up which I hope you check out!        </p>
       </div>
     </div>
   )
